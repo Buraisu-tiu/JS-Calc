c9f1d0bcdf9e7954a9ef2f43f30380ab74fcc4af
Buraisu Tieu <143369585+Buraisu-tiu@users.noreply.github.com>
2024-04-04 06:57:45 -0700

    Buttons
    Added the buttons for the keypad

---
+++ index.js
@@ -0,0 +1,62 @@
+let displayText = document.getElementById("display");
+console.log(displayText);
+
+function one() {
+  displayText.textContent += "1";
+}
+
+function two() {
+  displayText.textContent += "2";
+}
+
+function three() {
+  displayText.textContent += "3";
+}
+
+function four() {
+  displayText.textContent += "4";
+}
+
+function five() {
+  displayText.textContent += "5";
+}
+
+function six() {
+  displayText.textContent += "6";
+}
+
+function seven() {
+  displayText.textContent += "7";
+}
+
+function eight() {
+  displayText.textContent += "8";
+}
+
+function nine() {
+  displayText.textContent += "9";
+}
+
+function zero() {
+  displayText.textContent += "0";
+}
+
+function plus() {
+    displayText.textContent += "+";
+}
+
+function minus() {
+    displayText.textContent += "-";
+}
+
+function times() {
+    displayText.textContent += "x";
+}
+
+function divide() {
+    displayText.textContent += "/";
+}
+
+function erase() {
+    displayText.textContent = "";
+}
